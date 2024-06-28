43693ae059a1f27897efa27767f9e699bcff7ee6
isabella <costaferafael678@gmail.com>
2024-06-28 12:21:01 -0300

    aula 4

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ print ('#        ALGORITMOS EM PYTHON        #')
 print ('########################################')
 print ('# Escolha a opção desejada:            #')
 print ('#                                      #')
-print ('# 1.                                   #')
+print ('# 1.  Fibonacci                        #')
 print ('########################################')
+
+try:
+    choice = int(input())
+    
+    if choice == 1:
+        print('FIB')
+    else:
+        print('opção invalida!')
+except:
+        print('ocorreu um erro...')
---
+++ scripts/fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(qtt:int):
+    
+    sequence = [0,1]
+   
+    i = 2
+   
+    while i <qtt:
+       sequence.append(sequence[i-1] + sequence[i-2])
+       print(sequence)
+       i += 1
+    
+    return sequence
